27e025d57ab22e3a55ab2f1139fdf5a9160cbd02
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2026-03-15 02:35:48 +0600

    added new solutions

---
+++ UVa/10852/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 10001;
+bool prime[MAXN];
+int answer[MAXN];
+vector<int> primes;
+
+void preprocess() {
+    memset(prime, true, sizeof(prime));
+    memset(answer, -1, sizeof(answer));
+    primes.push_back(2);
+
+    for (int i = 3; i * i < MAXN; i += 2) {
+        if (prime[i] == true) {
+
+            for (int j = i * i; j < MAXN; j += i + i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 3; i < MAXN; i += 2) {
+        if (prime[i] == true) {
+            primes.push_back(i);
+        }
+    }
+
+    const int prime_len = primes.size();
+
+    for (int n = 2; n < MAXN; n++) {
+        for (int i = 0; i < prime_len; i++) {
+            int p = primes[i];
+
+            if (p > n) {
+                break;
+            }
+
+            int val = n - (p * (n / p));
+
+            if (answer[n] == -1) {
+                answer[n] = p;
+            } else {
+                if (val > (n - (answer[n] * (n / answer[n])))) {
+                    answer[n] = p;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int t, n;
+
+    preprocess();
+    scanf("%d", &t);
+
+    while (t--) {
+        scanf("%d", &n);
+        printf("%d\n", answer[n]);
+    }
+    return 0;
+}
---
+++ UVa/406/main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1001;
+int prime_count[MAXN];
+vector<int> primes;
+
+void preprocess() {
+    for (int i = 1; i < MAXN; i++) {
+        prime_count[i] = 1;
+    }
+
+    prime_count[0] = 0;
+    primes.push_back(1);
+
+    for (int i = 2; i < MAXN; i++) {
+        if (prime_count[i] == 1) {
+            primes.push_back(i);
+
+            for (int j = i + i; j < MAXN; j += i) {
+                prime_count[j] = 0;
+            }
+        }
+
+        prime_count[i] += prime_count[i - 1];
+    }
+}
+
+int main() {
+    int n, c;
+    preprocess();
+
+    while(scanf("%d %d", &n, &c) == 2) {
+        int num_of_primes = prime_count[n];
+        int primes_to_print = (num_of_primes % 2) == 0 ? 2 * c : 2 * c - 1;
+
+        printf("%d %d:", n, c);
+
+        if (primes_to_print >= num_of_primes) {
+            for (int i = 0; i < num_of_primes; i++) {
+                printf(" %d", primes[i]);
+            }
+        } else {
+            int i = (num_of_primes - primes_to_print) / 2;
+            int j = num_of_primes - i;
+
+            for (int k = i; k < j; k++) {
+                printf(" %d", primes[k]);
+            }
+        }
+
+        printf("\n\n");
+    }
+
+    return 0;
+}
